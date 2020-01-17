c5c969158508c5ec89d181852f8aaed0caf246aa
Alex Vitorino <halexsrv@gmail.com>
2020-01-17 13:06:47 -0300

    add form state for devs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './global.css';
 import './App.css';
@@ -6,30 +6,83 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      },
+    );
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
             <label htmlFor="github_username">Usuário</label>
-            <input name="github_username" id="github_username"></input>
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            />
           </div>
 
-          <div class="input-block">
+          <div className="input-block">
             <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs"></input>
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
           </div>
 
           <div className="input-group">
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude"></input>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
             </div>
 
-            <div class="input-block">
+            <div className="input-block">
               <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude"></input>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
             </div>
           </div>
 
@@ -39,7 +92,7 @@ function App() {
 
       <main>
         <ul>
-          <li class="dev-item">
+          <li className="dev-item">
             <header>
               <img
                 src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
@@ -53,7 +106,7 @@ function App() {
             <p>Plataforma de educação em tecnologia 🚀</p>
             <a href="https://github.com/Rocketseat">Acessar perfil no Github</a>
           </li>
-          <li class="dev-item">
+          <li className="dev-item">
             <header>
               <img
                 src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
@@ -67,7 +120,7 @@ function App() {
             <p>Plataforma de educação em tecnologia 🚀</p>
             <a href="https://github.com/Rocketseat">Acessar perfil no Github</a>
           </li>
-          <li class="dev-item">
+          <li className="dev-item">
             <header>
               <img
                 src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
