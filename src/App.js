e30ce4d368374af5df69d61773c2e7ed81b1d59f
Alex Vitorino <halexsrv@gmail.com>
2020-01-17 11:53:19 -0300

    added form layout to create devs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 
 import './global.css';
+import './App.css';
+import './Sidebar.css';
 
 function App() {
   return (
     <div id="app">
-      <aside></aside>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário</label>
+            <input name="github_username" id="github_username"></input>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs"></input>
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude"></input>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude"></input>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
       <main></main>
     </div>
   );
